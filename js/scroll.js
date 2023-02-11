39ca1fb5175c8c514305837eac888506a0bfb5c5
Amanda da Costa <122532141+mandscs@users.noreply.github.com>
2023-02-11 18:27:54 -0300

    alterações

--- js/scroll.js
+++
@@ -1,3 +0,0 @@
-window.sr = ScrollReveal({ reset:true});
-ScrollReveal({ distance: '15px' });
-sr.reveal('.fotoeu', {duration : 3000, origin: 'right'})
---
+++ js/scrollReveal.js
@@ -0,0 +1,9 @@
+window.sr = ScrollReveal({ reset: true})
+
+ScrollReveal().reveal (".resum", {
+    delay: 500
+
+})
+ScrollReveal().reveal (".sessao-conhecimentos",{
+    delay: 600
+})
