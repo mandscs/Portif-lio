725049ed17ace2b257ddb410c0dfc8040f29b88b
Amanda da Costa <122532141+mandscs@users.noreply.github.com>
2023-02-11 19:42:26 -0300

    ajust

--- js/scrollReveal.js
+++ js/scrollReveal.js
@@ -2,8 +2,10 @@ window.sr = ScrollReveal({ reset: true})
 
 ScrollReveal().reveal (".resum", {
     delay: 500
-
+})
+ScrollReveal().reveal (".spotify",{
+    delay: 700
 })
 ScrollReveal().reveal (".sessao-conhecimentos",{
-    delay: 600
+    delay: 700
 })
