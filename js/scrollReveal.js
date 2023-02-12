cca07788d5a20a2a58f10182efb36ce0372d6052
Amanda da Costa <122532141+mandscs@users.noreply.github.com>
2023-02-12 01:36:44 -0300

    alterações

--- js/scrollReveal.js
+++ js/scrollReveal.js
@@ -1,11 +1,20 @@
 window.sr = ScrollReveal({ reset: true})
 
+ScrollReveal().reveal (".banner h1", {
+    delay: 400
+})
+ScrollReveal().reveal (".banner p", {
+    delay: 700
+})
 ScrollReveal().reveal (".resum", {
-    delay: 500
+    delay: 400
+})
+ScrollReveal().reveal (".txteu",{
+    delay: 600
 })
 ScrollReveal().reveal (".spotify",{
-    delay: 700
+    delay: 400
 })
 ScrollReveal().reveal (".sessao-conhecimentos",{
-    delay: 700
+    delay: 400
 })
